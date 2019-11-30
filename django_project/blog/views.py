1a72550ceec11d33e8417c18d71c1d6f6f7fb444
metmirr <metin7087@gmail.com>
2019-11-30 13:27:30 +0300

    Add context to templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        "author": "Metin Demir",
+        "content": "First post content",
+        "title": "Blog Post 1",
+        "date_posted": "August 27, 2018",
+    },
+    {
+        "author": "Suleyman Akan",
+        "content": "Second post content",
+        "title": "Blog Post 2",
+        "date_posted": "August 28, 2018",
+    },
+]
+
+
 def home(request):
-    return render(request, "blog/home.html")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context=context)
 
 
 def about(request):
