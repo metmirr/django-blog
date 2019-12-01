b4f7b1f3e2d856023a5d90851aec17fa043b894d
metmirr <metin7087@gmail.com>
2019-12-01 13:29:22 +0300

    Add password change

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -20,7 +21,7 @@ class PostListView(ListView):
     template_name = "blog/home.html"
     context_object_name = "posts"
     ordering = ["-date_posted"]
-    paginate_by = 2
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
@@ -60,6 +61,17 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = "posts"
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
 
 
 def about(request):
